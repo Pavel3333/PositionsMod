fee0e59b09c23c4f9e39a49056db9c60f5a4e772
Pavel3333 <pavel.ushaev2012@yandex.ru>
2020-12-16 03:10:23 +0300

    add CMakeLists

--- build_x32_debug.bat
+++ build_x32_debug.bat
@@ -1,7 +1,6 @@
 chcp 1251
-rd /s /q "build_x86"
-mkdir build_x86
-cd build_x86
-cmake .. -DCMAKE_PREFIX_PATH="C:/WoTPython/Win32/" -DCMAKE_GENERATOR_PLATFORM=Win32 -DCMAKE_INSTALL_PREFIX="C:/Games/World_of_Tanks_RU/res_mods/mods/xfw_packages/Autoupdater_Main/native_32bit"
-cmake --build . --target INSTALL --config Debug
+rd /s /q "build/Win32"
+mkdir "build/Win32"
+cmake . -DCMAKE_PREFIX_PATH="C:/WoTPython/Win32/" -DCMAKE_GENERATOR_PLATFORM=Win32 -DCMAKE_INSTALL_PREFIX="deploy/Win32"
+cmake --build build/Win32 --target INSTALL --config Debug
 pause
--- build_x64_debug.bat
+++ build_x64_debug.bat
@@ -1,7 +1,6 @@
 chcp 1251
-rd /s /q "build_x64"
-mkdir build_x64
-cd build_x64
-cmake .. -DCMAKE_PREFIX_PATH="C:/WoTPython/x64/" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_INSTALL_PREFIX="C:/Games/World_of_Tanks_RU/res_mods/mods/xfw_packages/Autoupdater_Main/native_64bit"
-cmake --build . --target INSTALL --config Debug
+rd /s /q "build/x64"
+mkdir "build/x64"
+cmake . -DCMAKE_PREFIX_PATH="C:/WoTPython/x64/" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_INSTALL_PREFIX="deploy/x64"
+cmake --build build/x64 --target INSTALL --config Debug
 pause
